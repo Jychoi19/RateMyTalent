22bbab77eec6b0b2da7419836a39bbbd94f712e0
Joon Choi <jychoi19@gmail.com>
2016-01-20 00:09:09 +0900

    Added user authentication via email and password

--- www/js/app.js
+++ www/js/app.js
@@ -1,11 +1,4 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
-angular.module('RateMyTalent', ['ionic', 'RateMyTalent.controllers', 'RateMyTalent.services'])
+angular.module('RateMyTalent', ['ionic', 'RateMyTalent.controllers', 'RateMyTalent.services', 'firebase'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -23,74 +16,62 @@ angular.module('RateMyTalent', ['ionic', 'RateMyTalent.controllers', 'RateMyTale
   });
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
-
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
+.config(function($stateProvider, $urlRouterProvider) {  
+  $urlRouterProvider.otherwise('/signup');
   $stateProvider
-
-  // setup an abstract state for the tabs directive
-  .state('tab', {
-    url: '/tab',
-    abstract: true,
-    templateUrl: 'templates/tabs.html'
-  })
-
-  // Each tab has its own nav history stack:
-
-  .state('tab.mytalents', {
-    url: '/mytalents',
-    views: {
-      'tab-mytalents': {
-        templateUrl: 'templates/tab-mytalents.html',
-        controller: 'MyTalentsCtrl'
+    .state('signup', {
+      url: '/signup',
+      controller: 'SignupCtrl',
+      templateUrl: 'templates/signup.html'
+    })
+    .state('tab', {
+      url: '/tab',
+      abstract: true,
+      templateUrl: 'templates/tabs.html'
+    })
+    .state('tab.mytalents', {
+      url: '/mytalents',
+      views: {
+        'tab-mytalents': {
+          templateUrl: 'templates/tab-mytalents.html',
+          controller: 'MyTalentsCtrl'
+        }
       }
-    }
-  })
-
-  .state('tab.history', {
-    url: '/history',
-    views: {
-      'tab-history': {
-        templateUrl: 'templates/tab-history.html',
-        controller: 'HistoryCtrl'
+    })
+    .state('tab.history', {
+      url: '/history',
+      views: {
+        'tab-history': {
+          templateUrl: 'templates/tab-history.html',
+          controller: 'HistoryCtrl'
+        }
       }
-    }
-  })
-  
-  .state('tab.history-detail', {
-    url: '/history/:talentId',
-    views: {
-      'tab-history-detail': {
-        templateUrl: 'templates/history-detail.html',
-        controller: 'HistoryDetailCtrl'
+    })
+    .state('tab.history-detail', {
+      url: '/history/:talentId',
+      views: {
+        'tab-history-detail': {
+          templateUrl: 'templates/history-detail.html',
+          controller: 'HistoryDetailCtrl'
+        }
       }
-    }
-  })
-
-  .state('tab.ratings', {
-    url: '/ratings',
-    views: {
-      'tab-ratings': {
-        templateUrl: 'templates/tab-ratings.html',
-        controller: 'RatingsCtrl'
+    })
+    .state('tab.ratings', {
+      url: '/ratings',
+      views: {
+        'tab-ratings': {
+          templateUrl: 'templates/tab-ratings.html',
+          controller: 'RatingsCtrl'
+        }
       }
-    }
-  })
-
-  .state('tab.settings', {
-    url: '/settings',
-    views: {
-      'tab-settings': {
-        templateUrl: 'templates/tab-settings.html',
-        controller: 'SettingsCtrl'
+    })
+    .state('tab.settings', {
+      url: '/settings',
+      views: {
+        'tab-settings': {
+          templateUrl: 'templates/tab-settings.html',
+          controller: 'SettingsCtrl'
+        }
       }
-    }
-  });
-
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/mytalents');
-
+    });
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,5 +1,32 @@
 angular.module('RateMyTalent.controllers', [])
 
+.controller('SignupCtrl', function($scope, $state) {
+  $scope.user = {};
+  $scope.errors = null;
+  var ref = new Firebase("https://ratemytalent.firebaseio.com");
+
+  $scope.createAccount = function(){
+    if($scope.user.password === $scope.user.passwordConfirmation) {
+      ref.createUser({
+        email    : $scope.user.email,
+        password : $scope.user.password
+      }, function(error, userData) {
+        $scope.$apply(function(){
+          if (error) {
+            console.log(error);
+            $scope.errors = "" + error;
+          } else {
+            console.log("Successfully created user account with uid:", userData.uid);
+            $state.transitionTo("tab.mytalents");
+          }
+        });
+      });
+    } else {
+      $scope.errors = "Error: The passwords do not match.";
+    }
+  }
+})
+
 .controller('MyTalentsCtrl', function($scope) {})
 
 .controller('HistoryCtrl', function($scope, MyHistory) {
@@ -11,10 +38,10 @@ angular.module('RateMyTalent.controllers', [])
   //$scope.$on('$ionicView.enter', function(e) {
   //});
 
-  $scope.history = MyHistory.all();
-  $scope.remove = function(talent) {
-    MyHistory.remove(talent);
-  };
+$scope.history = MyHistory.all();
+$scope.remove = function(talent) {
+  MyHistory.remove(talent);
+};
 })
 
 .controller('HistoryDetailCtrl', function($scope, $stateParams, MyHistory) {
